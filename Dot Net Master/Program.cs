d6cfe7bc950c7f99d8c144f252f63df2d0139791
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-07 23:06:06 +0530

    String Builder

--- Dot Net Master/Program.cs
+++ Dot Net Master/Program.cs
@@ -1,18 +1,33 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         // Boxing 
 
-        int num = 23; // 23 will assign to num
+        //int num = 23; // 23 will assign to num
 
-        object Obj = num; // Boxing 
+        //object Obj = num; // Boxing 
 
-        int j = (int)Obj; // UnBoxing - Convert referance type to value type
+        //int j = (int)Obj; // UnBoxing - Convert referance type to value type
 
-        Console.WriteLine(num.GetType);
-        Console.WriteLine(Obj.GetType);
-        Console.WriteLine(j.GetType);
+        //Console.WriteLine(num.GetType);
+        //Console.WriteLine(Obj.GetType);
+        //Console.WriteLine(j.GetType);
+
+        StringBuilder stringBuilder = new StringBuilder("Kavinda");
+
+        stringBuilder.Append("Buddhika");
+        stringBuilder.Append("Bandara");
+        stringBuilder.Insert(7,"Bandara");
+        stringBuilder.Replace("Bandara","Shaku");
+
+        Console.WriteLine(stringBuilder);
+
+
+
+
 
         Console.ReadLine();
     }
