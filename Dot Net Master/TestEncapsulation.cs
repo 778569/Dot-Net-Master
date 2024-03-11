4db583d72a3610a09b0c59f56b36851235404a20
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-12 01:10:45 +0530

    Encapsulation

---
+++ Dot Net Master/TestEncapsulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Net_Master
+{
+    public class TestEncapsulation
+    {
+
+        private double _balance;  // private field representimng the internal state
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                _balance += amount;
+            }
+        }
+
+    public void Withdraw(double amount)
+        {
+            if (amount > 0 && _balance>=amount)
+            {
+                _balance -= amount;
+            }
+        }
+    }
+}
