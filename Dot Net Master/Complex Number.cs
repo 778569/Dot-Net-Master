dd897a309475c1f58f4cbc102458fb5b8452a816
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-12 01:42:28 +0530

    Operating overloading

---
+++ Dot Net Master/Complex Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Net_Master
+{
+    public class Complex_Number
+    {
+        public double Real { get; set; }
+
+        public double Imaginary { get; set; }
+
+        public Complex_Number(double real, double imaginary)
+        {
+                this.Real = real;
+                this.Imaginary = imaginary;
+        }
+
+        public static Complex_Number operator + (Complex_Number a, Complex_Number b)
+        {
+            return new Complex_Number(a.Real + b.Real, a.Imaginary + b.Imaginary);
+
+        }
+    }
+}
