8bf2fdc045bb1f757896723b8b2c60923d24d438
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-12 11:34:07 +0530

    Using linqs, stacks and properties

---
+++ Dot Net Master/Properties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Net_Master
+{
+    public class Properties
+    {
+        public string? Name;
+
+        public string name
+        {
+            get {
+                return name; 
+                }
+            set {
+                name = value; 
+                }
+        }
+
+        public int Age { get; set; }
+
+        public string? School { get; set; }
+    }
+}
