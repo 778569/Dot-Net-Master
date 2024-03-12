48cf395cee24561c409d02689a4b1c6bc072b6aa
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-12 09:26:03 +0530

    Base in method

--- Dot Net Master/Inharitance.cs
+++ Dot Net Master/Inharitance.cs
@@ -18,16 +18,28 @@ namespace Dot_Net_Master
         public Animal(int legs)
         {
             this.Legs = legs;
-            Console.WriteLine($"This animal has {legs}");
+            //Console.WriteLine($"This animal has {legs}");
         }
 
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("The animal make sound");
+        }
        
     }
     public class Dog : Animal
     {
         public Dog(int legs) : base(legs)
         {
-            Console.WriteLine("Your Dog has four leags");
+            //Console.WriteLine("Your Dog has four leags");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("The dog barks");
+            base.MakeSound();
+
+            Console.ReadLine();
         }
     }
 
